332b95f2ded5965ff1729a6afb99069fc1f4c82b
Edson Guerra <ecristianguerra@gmail.com>
2022-04-13 23:03:58 -0400

    Updates

---
+++ static/js/573.320684b1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfirst_react_app=self.webpackChunkfirst_react_app||[]).push([[573],{573:function(e,t,a){a.r(t),t.default={}}}]);
+//# sourceMappingURL=573.320684b1.chunk.js.map
---
+++ static/js/961.5f57b5bf.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfirst_react_app=self.webpackChunkfirst_react_app||[]).push([[961],{961:function(e,t,a){a.r(t),t.default={}}}]);
+//# sourceMappingURL=961.5f57b5bf.chunk.js.map
